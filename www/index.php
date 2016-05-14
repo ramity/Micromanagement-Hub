f47b25eceb02982d13470130d6e6ffbe8490edd0
ramity <lcb005@uark.edu>
2016-05-13 20:26:33 -0500

    Updating intial looks and navigation

---
+++ www/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+  <head>
+    <title>Ramity Hub</title>
+    <link rel="stylesheet" type="text/css" href="http://localhost/css/main.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div id="leftsidebar">
+      <div class="leftsidebaritem"></div>
+      <a href="">
+        <div class="leftsidebaritem">Dashboard</div>
+      </a>
+      <a href="">
+        <div class="leftsidebaritem">Social Hub</div>
+      </a>
+      <a href="">
+        <div class="leftsidebaritem">Food Tracker</div>
+      </a>
+      <a href="">
+        <div class="leftsidebaritem">Schedule Planner</div>
+      </a>
+      <a href="">
+        <div class="leftsidebaritem">Closet Inventory</div>
+      </a>
+    </div>
+    <div id="rightsidebar">
+      <a href="">
+        <div class="rightsidebaritem"></div>
+      </a>
+      <a href="">
+        <div class="rightsidebaritem"></div>
+      </a>
+      <a href="">
+        <div class="rightsidebaritem"></div>
+      </a>
+      <a href="">
+        <div class="rightsidebaritem"></div>
+      </a>
+    </div>
+    <div id="top">
+      <a href="http://localhost/">
+        <div id="toplogo">Ramity</div>
+      </a>
+      <div id="toplogin">
+        <a href="http://localhost/register.php">
+          <div class="toploginbutton">Register</div>
+        </a>
+        <a href="http://localhost/login.php">
+          <div class="toploginbutton">Login</div>
+        </a>
+      </div>
+    </div>
+    <div id="container">
+      <div id="containerinr">
+        <div class="containerheader">Development plan</div>
+      </div>
+    </div>
+  </body>
+</html>
